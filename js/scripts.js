92fb1955bad6a8603c7a28e7a11763a9ef08ed7a
Collin Person and Stephanie Mayer <stephanie@>
2017-07-05 16:27:40 -0700

    added js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
-var add= function(number1,number 3){return number1 + number2;
+var add = function(number1, number2) {
+  return number1 + number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"))
-alert(number1);
-
-alert(add(number1,number2));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+$(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
