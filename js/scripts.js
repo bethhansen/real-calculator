4df98582f8860ce034d51eee291bad289a22cbb1
Beth Hansen and Stephanie Mayer <beth@>
2017-07-06 08:42:04 -0700

    add multiply, divide

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,30 @@ var divide = function(number1, number2) {
 $(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
     var result = add(number1, number2);
     $("#output").text(result);
   });
-  $("form#add").submit(function(event) {
+  $("form#subtract").submit(function(event) {
     event.preventDefault();
-    var number1 =parseInt($("add1").val());
+    var number1 = parseInt($("#number3").val());
+    var number2 = parseInt($("#number4").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number5").val());
+    var number2 = parseInt($("#number6").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number7").val());
+    var number2 = parseInt($("#number8").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
   });
 });
